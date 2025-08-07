0d1790c54ebfc021b45db6cab71a382956a909d8
DoraStark <doraschneesturmin@example.com>
2025-08-07 13:20:29 +0200

    Add chalk usage example in index.js

---
+++ nodeBasics/index.js
@@ -0,0 +1,6 @@
+
+import chalk from 'chalk';
+
+console.log(chalk.green('Це кольоровий текст з бібліотеки Chalk!'));
+console.log(chalk.blue('Це кольоровий текст з бібліотеки Chalk!'));
+console.log(chalk.red('Це кольоровий текст з бібліотеки Chalk!'));
