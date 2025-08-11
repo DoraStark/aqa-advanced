6794641650d89ee78201a66ffdbc75afd15711b0
DoraStark <doraschneesturmin@example.com>
2025-08-11 14:46:15 +0200

    виправлений синтаксис

--- jsBasics/task3.js
+++ jsBasics/task3.js
@@ -4,10 +4,10 @@ let age1 = 2;
 
 let adult = age > 18;
 
-console.log ("Вік : ", age)
-console.log ("Повнолітній :", adult)
+console.log ("Вік : ", age);
+console.log ("Повнолітній :", adult);
 
 adult = age1 > 18;
 
-console.log ("Вік : ", age1)
-console.log ("Повнолітній :", adult)
+console.log ("Вік : ", age1);
+console.log ("Повнолітній :", adult);
