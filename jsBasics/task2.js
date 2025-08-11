37b387134233b2b2bebac6264c564d743198de82
DoraStark <doraschneesturmin@example.com>
2025-08-11 14:38:25 +0200

    JS Basics, домашка 4.1

---
+++ jsBasics/task2.js
@@ -0,0 +1,11 @@
+
+let name1 = "Пітер";
+let name2 = "Річард";
+
+
+let greetingConcat = "Привіт, " + name1 + " і " + name2 + "!";
+console.log(greetingConcat);
+
+
+let greetingTemplate = `Привіт, ${name1} і ${name2}!`;
+console.log(greetingTemplate);
---
+++ jsBasics/task4.js
@@ -0,0 +1,22 @@
+//4.1
+
+let radius = 5
+let areaCircle = Math.PI * Math.pow(radius, 2);
+console.log ("Площа: ", areaCircle.toFixed(2))
+
+//4.2
+let length = 10.456
+let width = 7.23
+let areaRectangle = length * width
+console.log("Площа прямокутника: ", areaRectangle.toFixed(2))
+
+
+
+//4.3
+
+let cylinderRadius = 9
+let height = 11
+let cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2)*height
+console.log("Об'єм циліндра: ", cylinderVolume.toFixed(2))
+
+
